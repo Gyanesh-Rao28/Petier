c3760aa76ce66f6e09b1b6d83e0e231ff244326b
Gyanesh Rao <gyaneshrao28@gmail.com>
2023-04-06 22:54:55 +0530

    init

--- js/menu.js
+++ js/menu.js
@@ -47,6 +47,96 @@ menuBtn.forEach((btn)=>{
   })
 })
 
+// menu 
+
+const menu = [
+  {
+    id: 1,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/dogs/dog1.jpg",
+    gender: `Female`,
+  },
+  {
+    id: 2,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/dogs/dog2.jpg",
+    gender: `Female`,
+  },
+  {
+    id: 3,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/dogs/dog3.jpg",
+    gender: `Female`,
+  },
+  {
+    id: 4,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/dogs/dog4.jpg",
+    gender: `Female`,
+  },
+  {
+    id: 5,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/dogs/dog5.jpg",
+    gender: `Female`,
+  },
+  {
+    id: 6,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/dogs/dog6.jpg",
+    gender: `Female`,
+  },
+  {
+    id: 7,
+    category: "dog",
+    breed: "DOG",
+    age: "2 yr",
+    location: "mumbai",
+    img: "./imgs/cat/cat1.jpg",
+    gender: `Female`,
+  },
+];
 
 
+const menuSection = document.getElementById("menu-grid");
 
+window.addEventListener("DOMContentLoaded",function (){
+
+let displayMenu = menu.map((item)=>{
+
+  return `<div class="menu-box">
+          <div class="box-img">
+            <img src=${item.img} class="photo">
+          </div>
+          <div class="menu-des">
+            <p>Breed - ${item.breed}</p>
+            <p>Age - ${item.age}</p>
+            <p>Location - ${item.location}</p>
+            <p>Gender - ${item.gender}</p>
+            <button class="menu-more"><img src="/imgs/pngs/arrow.png" style="transform: rotate(-180deg);"></button>
+          </div>
+        </div>`;
+
+});
+
+displayMenu = displayMenu.join("");
+menuSection.innerHTML = displayMenu;
+})
